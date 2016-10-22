c36ec57635105773e8812c510415189093e4a879
mehdi cherti <mehdi@cherti.name>
2016-10-22 21:10:59 +0200

    update

--- hp_toolkit/bandit.py
+++ hp_toolkit/bandit.py
@@ -6,89 +6,150 @@ from sklearn.utils import check_array, check_X_y
 import numpy as np
 
 from sklearn.preprocessing import FunctionTransformer
+from scipy.stats import norm
+
 
 class Bandit(object):
     
-    def __init__(self):
-        self.hist = []
-        self.actions = set()
-        self.nb_updates = 0
-
-    def update(self, action, reward):
-        raise NotImplementedError()
-    
-    def get_action_scores(self):
-        raise NotImplementedError()
-
-    def next(self):
-        scores = self.get_action_scores()
-        action, _ = max(scores.items(), key=lambda (action, score): score)
-        return action 
-
-class UCB(Bandit):
-    
-    def __init__(self):
-        super(UCB, self).__init__()
-        self.payoff_sums = defaultdict(lambda:0)
-        self.num_plays = defaultdict(lambda:0)
-        self.ucbs = defaultdict(lambda:0)
-
-    def update(self, action, reward):
-        self.actions.add(action)
-        t = self.nb_updates
-        self.num_plays[action] += 1
-        self.payoff_sums[action] += reward
-        self.nb_updates += 1
-
-    def get_action_scores(self):
-        assert self.nb_updates, 'You must provided at least one update iteration to get the next value'
-        t = self.nb_updates
-        po = self.payoff_sums
-        nplays = self.num_plays
-        actions = self.actions
-        ucbs = {a: po[a] / nplays[a] + upperBound(t, nplays[a])  for a in actions}
-        return ucbs
-
-def upperBound(step, nplays):
-    return math.sqrt(2 * math.log(step + 1) / nplays)
-
-class Thompson(Bandit):
-
     def __init__(self, model):
-        super(Thompson, self).__init__()
+        self.hist = []
         self.model = model
+        self.actions = set()
         self.hist_actions = []
         self.hist_rewards = []
-
-    def update(self, action, reward, partial_fit=False):
-        self.actions.add(action)
-        self.hist_actions.append(action)
-        self.hist_rewards.append(reward)
+    
+    def _update_actions(self, actions):
+        self.actions |= set(actions)
+    
+    def _update_model(self, actions, rewards, partial_fit=False):
+        self.hist_actions.extend(actions)
+        self.hist_rewards.extend(rewards)
         if partial_fit:
             assert hasattr(model, 'partial_fit')
-            self.model.partial_fit([action], [reward])
+            self.model.partial_fit(actions, rewards)
         else:
             a = self.hist_actions
             r = self.hist_rewards
             self.model.fit(a, r)
+    
+    def update(self, action, reward):
+        raise NotImplementedError()
+    
+    def get_action_scores(self, actions):
+        raise NotImplementedError()
 
-    def get_action_scores(self):
-        predict = self.model.sample_y if hasattr(self.model, 'sample_y') else self.model.predict
-        actions = list(self.actions)
+    def next(self, actions=None):
+        actions, scores = self.get_action_scores(actions=actions)
+        action, _ = max(zip(actions, scores), key=lambda (a,s):s)
+        return action
+
+def expected_improvement(model, X, hist_X=[], hist_y=[], xi=0.01):
+    # source : scikit optimize
+    y_opt = min(hist_y)
+    mu, std = model.predict(X, return_std=True)
+    values = np.zeros_like(mu)
+    mask = std > 0
+    improvement = y_opt - xi - mu[mask]
+    exploit = improvement * norm.cdf(improvement / std[mask])
+    explore = std[mask] * norm.pdf(improvement / std[mask])
+    values[mask] = exploit + explore
+    return values
+
+class BayesianOptimization(Bandit):
+    
+    def __init__(self, model, criterion=expected_improvement):
+        super(BayesianOptimization, self).__init__(model)
+        self.criterion = criterion
+ 
+    def update(self, actions, rewards):
+        self._update_actions(actions)
+        self._update_model(actions, rewards)
+
+    def get_action_scores(self, actions):
+        return self.criterion(self.model, actions, hist_X=self.hist_actions, hist_y=self.hist_rewards)
+
+class UCB(Bandit):
+    
+    def __init__(self, model):
+        super(UCB, self).__init__(model)
+        self.payoff_sums = defaultdict(lambda:0)
+        self.num_plays = defaultdict(lambda:0)
+
+    def update(self, actions, rewards):
+        self._update_actions(actions)
+        self._update_model(actions, rewards)
+        for a, r in zip(actions, rewards):
+            self._update_one(a, r)
+
+    def _update_one(self, action, reward):
+        t = len(self.hist_rewards)
+        self.num_plays[action] += 1
+        self.payoff_sums[action] += reward
+
+    def get_action_scores(self, actions=None):
+        actions = actions if actions else self.actions
+        return map(self._get_payoff, actions)
+
+    def _get_payoff(self, action):
+        po = self.payoff_sums
+        nplays = self.num_plays
+        a = action
+        t = len(self.hist_rewards)
+        if a in nplays:
+            avg_reward = po[a] / nplays[a]
+            ub = self._upperbound(t, nplays[a])
+        else:
+            avg_reward, std_reward = self.model.predict([a], return_std=True)
+            avg_reward = avg_reward[0]
+            ub = std_reward[0]
+        return avg_reward + ub
+
+    def _upperbound(self, step, nplays):
+        return math.sqrt(2 * math.log(step + 1) / nplays)
+
+class Simple(Bandit):
+    
+    def update(self, actions, rewards):
+        self._update_actions(actions)
+        self._update_model(actions, rewards)
+
+    def get_action_scores(self, actions=None):
+        actions = actions if actions else self.actions
+        predict = self.model.predict
+        actions = list(actions)
         rewards = predict(actions)
-        return {a: r for a, r in zip(actions, rewards)}
+        return rewards
+
+
+class Thompson(Bandit):
+    
+    def update(self, actions, rewards):
+        self._update_actions(actions)
+        self._update_model(actions, rewards)
+
+    def get_action_scores(self, actions=None):
+        actions = actions if actions else self.actions
+        predict = self.model.sample_y
+        actions = list(actions)
+        rewards = predict(actions)
+        return rewards
 
 if __name__ == '__main__':
     from sklearn.linear_model import LinearRegression
     from sklearn.pipeline import make_pipeline
     from sklearn.gaussian_process import GaussianProcessRegressor
     from sklearn.preprocessing import Imputer
-    from helpers import DictVectorizer
+    from helpers import DictVectorizer, Pipeline
     from frozendict import frozendict
     fd = frozendict
-    gp = make_pipeline(DictVectorizer(), Imputer(), GaussianProcessRegressor())
+    gp = Pipeline(DictVectorizer(), Imputer(), GaussianProcessRegressor(normalize_y=True))
     bdt = Thompson(gp)
-    bdt.update(fd({'a': 1, 'b': 3}), 5)
-    bdt.update(fd({'a': 2}), 5)
-    bdt.update(fd({'a': 3, 'b': 4}), 8)
-    print(bdt.get_action_scores())
+    x = [
+        fd({'a': 1, 'b': 2}),
+        fd({'a': 2, 'b': 3}),
+        fd({'a': 5, 'b': 4})
+    ]
+    y = [3, 5, 9]
+    bdt.update(x, y)
+    actions = x + [fd({'a': 6, 'b': 4})]
+    print(bdt.get_action_scores(actions=actions))
--- hp_toolkit/helpers.py
+++ hp_toolkit/helpers.py
@@ -1,26 +1,7 @@
 import collections
 import numpy as np
 import pandas as pd
-
-class DictVectorizer(object):
-    
-    def __init__(self, colnames=None, missing_values='NaN'):
-        self.colnames = set(colnames) if colnames else set()
-        self.missing_values = np.nan if missing_values == 'NaN' else missing_values
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X = map(flatten_dict, X)
-        X = pd.DataFrame(X)
-        self.colnames |= set(X.columns)
-        for c in self.colnames - set(X.columns):
-            X[c] = self.missing_values
-        X = np.array(X)
-        if len(X.shape) == 1:
-            X = X[:, np.newaxis]
-        return X
+from sklearn.preprocessing import FunctionTransformer
 
 def flatten_dict(l):
     d = {}
@@ -29,8 +10,46 @@ def flatten_dict(l):
             d.update(flatten_dict(v))
         elif isinstance(v, list) or isinstance(v, tuple):
             for i, l in enumerate(v):
-                d[k+'_{}'.format(i)] = l
+                if not isinstance(l, collections.Mapping):
+                    d[k+'_{}'.format(i)] = l
+                else:
+                    for e in v:
+                        d.update(flatten_dict(e))
         else:
             d[k] = v
     return d
+
+DictFlattener = FunctionTransformer(flatten_dict)
+
+class Pipeline(object):
+    
+    def __init__(self, *steps):
+        self.steps = steps
+    
+    def fit_transform(self, X, y=None):
+        self.fit(X, y=y)
+        return self.transform(X, y=y)
+
+    def fit(self, X, y=None):
+        steps = self.steps
+        for step in steps[0:-1]:
+            X = step.fit_transform(X, y=y)
+        return steps[-1].fit(X, y=y)
+
+    def transform(self, X):
+        steps = self.steps
+        for step in steps[0:-1]:
+            X = step.transform(X)
+        return X
+    
+    def predict(self, X, *args, **kwargs):
+        X = self.transform(X)
+        y = self.steps[-1].predict(X, *args, **kwargs)
+        return y
+
+    def sample_y(self, X, *args, **kwargs):
+        X = self.transform(X)
+        y = self.steps[-1].sample_y(X, *args, **kwargs)
+        return y
+
 
